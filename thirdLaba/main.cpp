2e33fa343926474af64ed5aa338760ad1eaa2733
Denis Chirets <denischirets@gmail.com>
2024-11-27 19:16:19 +0300

    доведена до ума 3-я лаба

--- thirdLaba/main.cpp
+++ thirdLaba/main.cpp
@@ -8,6 +8,7 @@
 #include <mutex>
 #include <csignal>
 #include <cstdlib> 
+#include <fstream>
 
 using namespace std;
 
@@ -17,7 +18,7 @@ struct MarkerData {
     int                signalToMain;
     int                shouldContinue;
     int*               array;
-    pthread_mutex_t*   sharedMutex;//statoc?
+    pthread_mutex_t*   sharedMutex;//static?
     pthread_mutex_t*   personalMutex;
     pthread_cond_t*    condForBegin;//static?
     pthread_cond_t*    personalCondition;
@@ -61,6 +62,11 @@ struct MarkerData {
         arraySize = obj->arraySize;
         sharedMutex = obj->sharedMutex;
         condForBegin = obj->condForBegin;
+        personalCondition = obj->personalCondition;
+        shouldContinue = obj->shouldContinue;
+        signalToMain = obj->signalToMain;
+        personalMutex = obj->personalMutex;
+        isActive = obj->isActive;
         return *this;
     }
 
@@ -72,46 +78,47 @@ struct MarkerData {
 
 void* markerThread(void* a){
     // cout << a << endl;
-    MarkerData threadData = *(MarkerData*)a;
-    pthread_mutex_lock(threadData.personalMutex);
-    cout << threadData.threadId << "-ый маркер захватил мьютекс" << endl;
-    threadData.signalToMain = 1;
-    pthread_cond_wait(threadData.condForBegin, threadData.personalMutex);
-    pthread_mutex_unlock(threadData.personalMutex);
+    MarkerData* threadData= (MarkerData*)a;
+    pthread_mutex_lock(threadData->personalMutex);
+    cout << threadData->threadId << "-ый маркер захватил личный мьютекс" << endl;
+    threadData->signalToMain = 1;
+    pthread_cond_wait(threadData->condForBegin, threadData->personalMutex);
+    pthread_mutex_unlock(threadData->personalMutex);
     
 
-    srand(threadData.threadId+1);
+    srand(threadData->threadId+1);
 
-    pthread_mutex_lock(threadData.sharedMutex);
-    while (threadData.shouldContinue)
+    pthread_mutex_lock(threadData->sharedMutex);
+    cout << threadData->threadId << "-ый маркер захватил общий мьютекс" << endl;
+    while (threadData->shouldContinue)
     {
         int randomValue  = rand();
         int numberOfMarkedElements(0);
-        randomValue %= threadData.arraySize+1;
-        pthread_mutex_lock(threadData.sharedMutex);
-        if(threadData.array[randomValue] == 0){
-            sleep(1);
-            threadData.array[randomValue] = threadData.threadId+1;
+        randomValue %= threadData->arraySize+1;
+        //pthread_mutex_lock(threadData->sharedMutex);
+        if(threadData->array[randomValue] == 0){
+            usleep(5);
+            threadData->array[randomValue] = threadData->threadId;
+            // cout << threadData->threadId << "-ый маркер красит " << randomValue << "-ый элемент" << endl;
             numberOfMarkedElements++;
-            sleep(1);
+            usleep(5);
         }
         else
         {
-            cout << "Поток номер " << threadData.threadId+1 << "покрасил" << numberOfMarkedElements << "элементов" 
-                 << "приостановив работу на " << randomValue << "-ом элементе" << endl;
-            threadData.signalToMain = 1;
-            pthread_cond_wait(threadData.personalCondition, threadData.sharedMutex);
+            cout << "Поток номер " << threadData->threadId << " приостановил работу на " 
+                 << randomValue << "-ом элементе" << endl;
+            threadData->signalToMain = 1;
+            pthread_cond_wait(threadData->personalCondition, threadData->sharedMutex);
         }
-        
-        pthread_mutex_unlock(threadData.sharedMutex);
     }
 
-    pthread_mutex_lock(threadData.sharedMutex);
-    for(int i = 0; i < threadData.arraySize; i++){
-        if(threadData.array[i] == threadData.threadId+1)
-            threadData.array[i] = 0;
+    // pthread_mutex_unlock(threadData->sharedMutex);
+    // pthread_mutex_lock(threadData->sharedMutex);
+    for(int i = 0; i < threadData->arraySize; i++){
+        if(threadData->array[i] == threadData->threadId)
+            threadData->array[i] = 0;
     }
-    pthread_mutex_unlock(threadData.sharedMutex);
+    pthread_mutex_unlock(threadData->sharedMutex);
 
     return 0;
 };
@@ -119,10 +126,12 @@ void* markerThread(void* a){
 
 int main() {
 
+
+    ifstream fin("input.txt");
     int error;
     int n;
     cout << "Enter the number of elements in the array: ";
-    cin >> n;
+    fin >> n;
     int* arr = new int[n];
 
     for(int i = 0; i < n; i++){
@@ -131,7 +140,7 @@ int main() {
 
     int numberOfThreads;
     cout << "Enter the numer of threads: ";
-    cin >> numberOfThreads;
+    fin >> numberOfThreads;
 
     pthread_cond_t    condforbegin  = PTHREAD_COND_INITIALIZER;
     pthread_mutex_t   mtx           = PTHREAD_MUTEX_INITIALIZER;
@@ -139,7 +148,7 @@ int main() {
     MarkerData* markerData =  new MarkerData[numberOfThreads];
     pthread_t* pthreads =  new pthread_t[numberOfThreads];
     for(int i = 0; i < numberOfThreads; i++){
-        markerData[i] = new MarkerData(&mtx, i , arr, n, &condforbegin);
+        markerData[i] = new MarkerData(&mtx, i+ 1, arr, n, &condforbegin);
         if(pthread_create(pthreads + i, NULL, markerThread, markerData + i)){
             cout << "ошибка при создании потока" << endl;
         }
@@ -153,6 +162,7 @@ int main() {
             sum += markerData[i].signalToMain;
             pthread_mutex_unlock(markerData[i].personalMutex);
         }
+        cout << sum << "потоков закончило приготоления"  << endl;
         if(sum == numberOfThreads)
             break;
         sleep(1);
@@ -162,7 +172,8 @@ int main() {
     for(int i = 0; i < numberOfThreads; i++){
             markerData[i].signalToMain = 0;
     }
-
+    
+    cout << "потокам дана команда продолжить" << endl;
 
     pthread_cond_broadcast(&condforbegin);//bigining of the actual work of threads
 
@@ -180,32 +191,49 @@ int main() {
             sleep(1);
         }
 
-        cout << "массив: ";
+        cout << "Массив: ";
         for(int i = 0; i < n; i++){
             cout << arr[i] << " ";
         }
+        cout << endl;
 
         int currentThread;
-        cout << "с каким маркером работаем?"  << endl;
+        cout << "C каким маркером работаем?"  << endl;
         cin >> currentThread; 
         currentThread--;
-        if(markerData[currentThread].isActive == false){
+        while(markerData[currentThread].isActive == false){
             cout << "поток неактивен, попробуйте ещё раз" << endl;
-            continue;
+            cin >> currentThread;
+            currentThread--;
         }
-        markerData[currentThread].isActive = false;
+        
+        NumberOfActiveThreads--;
+        markerData[currentThread].isActive = 0;
+        markerData[currentThread].shouldContinue = false;
         pthread_cond_signal(markerData[currentThread].personalCondition);   
 
-        NumberOfActiveThreads--;
+        void* d;
+        pthread_join(pthreads[currentThread], &d);
+
+        for(int i = 0; i < numberOfThreads; i++){
+            markerData[i].signalToMain = 1 - markerData[i].isActive;//гений читабельности
+        }
+
+
+        for(int i = 0; i < numberOfThreads; i++)
+            if(markerData[i].isActive)
+                pthread_cond_signal(markerData[i].personalCondition);
+
+
+        cout << "Массив: ";
+        for(int i = 0; i < n; i++){
+            cout << arr[i] << " ";
+        }
+        cout << endl;
 
     }
 
 
-
-    void* d;
-    for(int i = 0; i < numberOfThreads; i++){
-        pthread_join(*(pthreads+i), &d);
-    }
     pthread_mutex_destroy(&mtx);
 
     return 0;
